d799e5798c95af8760bcc9da97a8b5d9bfe7eb5c
Michał Ziółecki <michal.ziolecki.lp@gmail.com>
2021-01-21 23:41:17 +0100

    checking image source content-type

--- scraper/scraper_src/img_scraper.py
+++ scraper/scraper_src/img_scraper.py
@@ -1,7 +1,11 @@
 from asyncio import Lock
 
+import requests
 from bs4 import BeautifulSoup
 from bs4.element import Tag
+from requests import Response
+from rest_framework.status import HTTP_200_OK
+
 from scraper_src.abstract_scraper import AbstractScraper
 
 
@@ -13,22 +17,39 @@ class ImageScraper(AbstractScraper):
     def run_scraping(self, content):
         soup = BeautifulSoup(content, 'html.parser')
         images = soup.find_all('img')
-        print(f' images={images}')
         images_db = ImageScraper.prepare_linked_images(images)
         ImageScraper.save_images_in_db(images_db)
 
     @staticmethod
     def prepare_linked_images(images) -> list:
+
+        def check_source(link: str, prefix='') -> tuple:
+            link = f'{prefix}{link}'
+            response: Response = requests.get(link)
+            content_type: str = response.headers.get('Content-Type')
+            if response.status_code == HTTP_200_OK and 'image' in content_type:
+                return True, link
+            return False, link
+
         images_list = []
         for image in images:
             image: Tag
             alt = image.get('alt')
             src = image.get('src')
-            print(f'0source={src}')
+            correct_source = False
             if src and src.startswith('//'):
-                src = src[2:]
+                correct_source, src = check_source(src, 'https:')
+                if not correct_source:
+                    correct_source, src = check_source(src, 'http:')
+            elif src.startswith('http'):
+                correct_source, src = check_source(src)
+            else:
+                correct_source, src = check_source(src, 'https://')
+                if not correct_source:
+                    correct_source, src = check_source(src, 'http://')
             # TODO create database entity and append to list
-            images_list.append(image)
+            if correct_source:
+                images_list.append(image)
         return images_list
 
     @staticmethod
